2b2a139cb6a1df6e1e2b6bf9b2dc67099acf1d70
kanika agarwal <kanikaagarwal556@gmail.com>
2026-05-23 23:24:32 +0530

    Add day 1 cppp code

---
+++ day 1 practice.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a= 14;
+    int b = 15;
+cout<<"this is my day one of practice.\n the value of a is"<<a<<".\n the value of b is"<<b;
+ return 0;
+}
