d6c56a062f85cc4a877f7d6baf785599c666074f
kanika agarwal <kanikaagarwal556@gmail.com>
2026-05-24 11:06:21 +0530

    Add 2 day cpp code

---
+++ 2 day practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Don't give up
+int main() {
+     int a = 1
+     int b = 2;
+    int c = 4 , d = 5;
+    float pi = 3.14;
+    float e = 2.71;
+    char k = 'r' , K = 's';
+    cout << "This is the second day of practice.\nthe value of a is " <<a<< ".\nThe value of b is "<<b;
+    cout << ".\nthe value of pi is "<<pi;
+    cout << ".\nThe value of k is "<<k<<".\nThe value of K is "<<K<< ".";
+    return 0;
+
+     
+}
