bf3e97eb2683f400d87f54e5bfac58a5fa070f34
kanika agarwal <kanikaagarwal556@gmail.com>
2026-05-25 11:45:32 +0530

    Add day 3 cpp codes

---
+++ day 3 practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Entre the value of num1:";
+    cin>>num1;
+    cout<<"Entre the value of num2:";
+    cin>>num2;
+    cout<< "Sum= "<<num1+num2;
+    return 0;
+
+}
