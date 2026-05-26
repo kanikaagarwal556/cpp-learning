3c0c4018d3f25a50f1c60784f36445091bb30946
kanika agarwal <kanikaagarwal556@gmail.com>
2026-05-26 23:54:37 +0530

    Add 4 day cppcode

---
+++ 4 day.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+//The mistake i made at the first day
+//I write <isostream>istead of <iostream>
+// I write int a =15 above int main(){
+//I mistake in runing command
+//I am using backslash thinking its forwardslash
+//forget to write std
+//forget to close my lines with ; where needed
+//forget to save code before runing
+
+using namespace std;
+int main(){
+int a = 4 , b = 5;
+cout<< "A + B is "<< a + b;
+return 0;
+
+
+}
+
+
+
+
+
+
+
