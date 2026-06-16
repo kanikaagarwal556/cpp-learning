7ce181a84eab86e82f73e6b422b33b58ec03dd1e
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-16 22:47:25 +0530

    Add day 5 cpp code

---
+++ day5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a = 5 , b = 5;
+cout<< "The value of a+b is "<< a+b <<endl;
+cout << "The value of a-b is " << a-b << endl;
+cout << "The value of a*b is " << a*b << endl;
+cout << "The value of a/b is "<< a/b << endl;
+cout << "The value of a++ is "<< a++ << endl;
+cout << "The value of a-- is " << a--<< endl;
+cout << "The value of ++a is " << ++a << endl;
+cout << "The value of --a is " << --a << endl;
+return 0;
+}
+
