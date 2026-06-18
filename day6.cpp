dfcf2ebf73520329570c73196b86c18e2f96a84a
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-18 09:20:48 +0530

    Add day 6 cpp code

---
+++ day6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b = 7 ;
+    float c = 2.2 ;
+    cout << "the value of a+b-c+b*a is " << (a+(b-(c+(b*a))));
+return 0;
+
+}
