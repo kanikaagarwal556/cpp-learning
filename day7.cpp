70b1eb4c12c2a3aaab9907c2503d5bd52a42242c
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-19 08:06:11 +0530

    Add day 7 cpp code

---
+++ day7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b =9;
+    cout<<"The value of a==b is "<<(a==b)<<endl;
+    cout<<"The value of a!=b is "<<(a!=b)<<endl;
+    cout<<"The value of a<=b is "<<(a<=b)<<endl;
+    cout<<"The value of a=>b is "<<(a>=b)<<endl;
+    cout<<"The value of a<b is "<<(a<b)<<endl;
+    cout<<"The value of a>b is "<<(a>b)<<endl;
+    return 0;
+    
+
+
+
+}
