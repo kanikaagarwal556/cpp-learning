61afe9792c23cb8c60f06ba75f72503df133c2b5
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-19 08:08:59 +0530

    Add day 8 cpp code

---
+++ day8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+//int a = 2, b = 6 , g = 7;
+int c = 5;
+using namespace std;
+
+int main(){
+    // int a = 7 , b = 6 , g = 8;
+     // cout<< "The value of a is "<< a <<endl;
+    // cout<< "The value of b is "<< b <<endl;
+    // cout<< "The value of g is "<< g <<endl;
+    // cout<< "The value of a is "<< ::a <<endl;
+    // cout<< "The value of b is "<< ::b<<endl;
+    // cout<< "The value of g is "<< ::g <<endl;
+    int k , p ;
+    
+    
+    cout << "The value of K is "<< endl;
+    cin>> k;
+    cout <<"The value of P is "<< endl;
+    cin>> p;
+    int c = k+p;
+    cout<<"The value of c is " <<c<< endl;
+    
+    cout<< "The value of global varible c is " <<::c<<endl;
+    cout
+
+    
+
+
+
+    return 0;
+   
+
+
+}
+
+
+
+
