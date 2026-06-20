f01bee465f7ec9f4d71078607ba94560f566976d
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-20 12:21:04 +0530

    Add day 9 cpp code

---
+++ day9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+// float and long doublr varible
+int main(){
+float d = 6.2 ;
+long double e = 100;
+cout <<" the size of 100 is " << sizeof (100)<<endl;
+cout <<" the size of 6 is " << sizeof (e)<<endl;
+cout <<" the size of 100l is " << sizeof (100l)<<endl;
+cout <<" the size of 100L is " << sizeof (100L)<<endl;
+cout <<" the size of 6.2F is " << sizeof (6.2F)<<endl;
+cout <<" the size of 6.2f is " << sizeof (6.2f)<<endl;
+cout <<" the size of 6.2 is " << sizeof (6.2)<<endl;
+cout <<" the size of d is " << sizeof (d)<<endl;
+return 0;
+
+}
+    
+
+
+
+
+
