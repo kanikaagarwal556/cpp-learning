14a21f6fd0afda48c3fa201d6d36c6092b63d871
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-21 17:42:13 +0530

    Add day 10 cpp code

---
+++ day10.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //REFRENCE VARIABLE
+//     int a = 7;
+//     int & h = a;
+//     cout<<"The value of a is " << a <<". \nThe value of h is "<<h <<".";
+
+//typecasting
+int a = 7;
+float b = 8.5;
+cout<< "The value of (float) a is "<<(float)a<<endl;
+cout<< "The value of float(a)  is "<<float (a)<<endl;
+cout<< "The value of (int)b is "<<(int)b<<endl;
+cout<< "The value of int (b) is "<<int(b)<<endl;
+cout<< "The value of a+b is "<< a+b <<endl;
+cout<< "The value of a+(int) b is "<< a+(int)b <<endl;
+cout<< "The value of a+int (b) is "<< a+int(b) <<endl;
+cout<< "The value of (int) a+ b is "<< (int)a+b <<endl;
+cout<< "The value of int(a)+ b is "<< int(a)+b <<endl;
+cout<< "The value of a+float(b) is "<<a+float(b) <<endl;
+cout<< "The value of int(a) + float(b)is "<<(int)a+float(b) <<endl;
+cout<< "The value of int(b) + float(a)is "<<(int)b+float(a) <<endl;
+
+
+
+
+
+
+     return 0;
+
+
+
+
+}
