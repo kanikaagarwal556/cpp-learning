2b222e1ceaaa80078368a2f05749197270a9c9b7
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-22 11:37:37 +0530

    Add day 11 cpp code

---
+++ day11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = 55;
+    // cout << "The value of a is "<<a<<endl;
+    // a = 6;
+    // cout << "The value of a is "<<a<<endl;
+
+    //constant that use in cpp
+    const int a = 6;
+    cout << "The value of a is " << a << endl;
+    a = 5 ; // you will get a error because a is constant
+    cout << "The value of a is " << a << endl;
+    
+
+return 0;
+}
