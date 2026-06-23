818a8b34e2d0a14937fd23e8fdf1fe39aac19aa9
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-23 23:29:27 +0530

    Add day 12 cpp code

---
+++ day12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using  namespace std;
+
+int main(){
+    int a = 6, b = 78, c = 877 , d = 6577 , e = 23487;
+    cout<< "The value of a is: "<< setw(5) << a << endl;
+    cout<< "The value of b is: "<< setw(5) << b << endl;
+    cout<< "The value of c is: "<< setw(5) << c << endl;
+    cout<< "The value of d is: "<< setw(5) << d << endl;
+    cout<< "The value of e is: "<< setw(5) << e << endl;
+
+    return 0;
+
+
+
+}
