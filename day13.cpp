06d4fab767f06227ec48e80b4d4ba721b107d1f9
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-24 23:53:53 +0530

    Add day 13 cpp code

---
+++ day13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age ;
+    cout<< "Tell me your age "<<endl;
+    cin>> age ;
+    if (age > 100){
+        cout<<"You can come to the 'PARTY WITH THE BOOTHNATH' party."<<endl;
+         }
+         else if (age == 100){
+        cout<<"You are a kid and you can get a kid pass for 'PARTY IN THE HELL'."<<endl;
+        }
+    else{
+        cout << "You can not come to the party"<<endl;
+    
+    }
+return 0;
+
+
+}
