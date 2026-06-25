d30f277ef1ad8cc92bc049b66da1a66b566bfece
kanika agarwal <kanikaagarwal556@gmail.com>
2026-06-25 23:07:07 +0530

    Add day 14 cpp code

---
+++ day14.cpp
@@ -0,0 +1,32 @@
+#include  <iostream> 
+using namespace std;
+//for loops update part (i++ or ++i)runs alone,as a standards statement.
+//nobody is using its return value - the just need i ti become i+1,and both do excatly that
+
+
+int main(){
+//     for (int i = 6; i <= 10; ++i){
+// {cout << "i = "<<i<<endl;
+//}
+// for (int i = 7; i<=70; i*7 ){
+//     cout << "7*1= "<<i<<i*7<<endl;
+//     cout << "7*2= "<<i<<i*7<<endl;
+//     cout << "7*3= "<<i<<i*7<<endl;
+//     cout << "7*4= "<<i<<i*7<<endl;
+//     cout << "7*5= "<<i<<i*7<<endl;
+//     cout << "7*6= "<<i<<i*7<<endl;
+//     cout << "7*7= "<<i<<i*7<<endl;
+//     cout << "7*8= "<<i<<i*7<<endl;
+//     cout << "7*9= "<<i<<i*7<<endl;
+//     cout << "7*10= "<<i<<i*7<<endl;
+//     }
+
+for (int i=1 ; i<=10 ; i++ ){
+    cout<<"7*"<<i<<"=" << 7*i<<endl;
+}
+
+ return 0;
+}
+
+
+
